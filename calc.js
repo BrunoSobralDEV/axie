5216f85a7ba7987e45e9c599cbf7dd331878c531
Bruninho@27103045 <brunosobralss@hotmail.com>
2021-12-08 00:08:17 -0300

    function start

--- calc.js
+++ calc.js
@@ -1,8 +1,17 @@
-function insert(num) {
-    let numero = document.getElementById('resultado').innerHTML;
-    document.getElementById('resultado').innerHTML = numero +num;
-}
 
+function insert(num) {
+    //Criar e inserir o li no HTML
+    let ulHistory = document.querySelector('.history')
+    let addLi = document.createElement('li')
+    addLi.innerHTML = num
+
+    ulHistory.prepend(addLi)
+
+    //contagem
+    let points = document.querySelector('.actual')
+    points.innerHTML = num
+    console.log(points)
+}
 function start() {
     clean()
     document.getElementById('resultado').innerHTML = '+3';
@@ -13,7 +22,8 @@ function next() {
     document.getElementById('resultado').innerHTML = numero + '+2';
 }
 function clean() {
-    document.getElementById('resultado').innerHTML = '';
+    //document.getElementById('resultado').innerHTML = '';
+    document.querySelector('.history').innerHTML = '';
 }
 function calcular() {
     let resultado = document.getElementById('resultado').innerHTML;
